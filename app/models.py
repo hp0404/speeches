da4059d1a709b6875682b51faec0684005e6c6ed
hp0404 <hryhorii.pavlenko@gmail.com>
2021-12-14 17:36:27 +0200

    fixes #5

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import uuid
 from typing import Optional
 from datetime import datetime, date
 
@@ -6,14 +7,22 @@ from pydantic import HttpUrl
 from sqlmodel import Field, SQLModel
 
 
+def custom_uuid() -> uuid.UUID:
+    # Note: Work around UUIDs with leading zeros: https://github.com/tiangolo/sqlmodel/issues/25
+    val = uuid.uuid4()
+    while val.hex[0] == "0":
+        val = uuid.uuid4()
+    return val
+
+
 class SchemaBase(SQLModel):
-    id: Optional[int] = Field(
-        default=None,
+    id: uuid.UUID = Field(
         primary_key=True,
         index=True,
-        nullable=False
+        nullable=False,
+        default_factory=custom_uuid,
     )
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=datetime.utcnow, index=False)
 
 
 class Speeches(SchemaBase, table=True):
