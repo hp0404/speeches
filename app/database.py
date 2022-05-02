4b186f354d1fcf47827e31fe6efb07eecc652aa8
hp0404 <hryhorii.pavlenko@gmail.com>
2022-05-02 17:23:19 +0300

    yield sessions

--- app/database.py
+++ app/database.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
-from sqlmodel import create_engine
+import typing
+
+from sqlmodel import Session, create_engine
+
 from app.core.config import settings
 
 engine = create_engine(str(settings.DATABASE_URI))
+
+
+def get_session() -> typing.Iterator[Session]:
+    """Yields session"""
+    with Session(engine) as session:
+        yield session
