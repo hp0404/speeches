fbc4c32b2c959ca93769b494f59386f1db94f8a0
hp0404 <hryhorii.pavlenko@gmail.com>
2022-05-15 12:42:19 +0300

    added relationships

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ import typing
 import datetime
 
 from pydantic import HttpUrl
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 from sqlalchemy import Column, Integer
 from sqlalchemy.dialects import postgresql
 
@@ -20,17 +20,7 @@ def custom_uuid() -> uuid.UUID:
         val = uuid.uuid4()
     return val
 
-
-class ParsedText(SQLModel):
-    """Input fields that POST /speeches/ endpoint expects."""
-
-    title: str
-    text: str
-    date: datetime.date
-    URL: HttpUrl
-    category: typing.Optional[str] = Field(default=None)
-
-
+# SQLModel tables
 class Metadata(SQLModel, table=True):
     """public.metadata schema."""
 
@@ -47,6 +37,19 @@ class Metadata(SQLModel, table=True):
     date: datetime.date
     URL: HttpUrl
     category: typing.Optional[str] = Field(default=None)
+    
+    # Relationship
+    features: typing.List["Features"] = Relationship(
+        back_populates="meta",
+        sa_relationship_kwargs={
+            "primaryjoin": "Metadata.id==Features.document_id", 
+        }
+    )
+    text: "Texts" = Relationship(
+        back_populates="meta",
+        # one-to-one relationship 
+        sa_relationship_kwargs={"uselist": False}
+    )
 
 
 class Texts(SQLModel, table=True):
@@ -60,6 +63,12 @@ class Texts(SQLModel, table=True):
         foreign_key="metadata.id",
     )
     text: str = Field(index=False)
+    
+    # Relationship
+    meta: Metadata = Relationship(
+        back_populates="text",
+        sa_relationship_kwargs={"uselist": False}
+    )
 
 
 class Features(SQLModel, table=True):
@@ -80,46 +89,45 @@ class Features(SQLModel, table=True):
     # https://github.com/tiangolo/sqlmodel/issues/178#issuecomment-1044569342
     location: typing.List[int] = Field(sa_column=Column(postgresql.ARRAY(Integer())))
 
-
-class FeatureTypes(str, enum.Enum):
-    """Expected feature types."""
-
-    NE = "NE"
-    NP = "NP"
+    # Relationship
+    meta: Metadata = Relationship(back_populates="features")
 
 
-class FeaturePayload(SQLModel):
-    """Input fields that POST /features/ endpoint expects."""
+# SQLModels
+class ParsedText(SQLModel):
+    """Input fields that POST /speeches/ endpoint expects."""
 
-    text: str
-
-
-class _FeatureResponse(SQLModel):
-    """Single feature response fields."""
-
-    feature_id: int
-    document_id: uuid.UUID
-    feature_type: FeatureTypes
-    feature_label: str
-    match: str
-    match_normalized: str
-    location: typing.List[int]
-
-
-class FeatureResponse(SQLModel):
-    """Features response fields."""
-
-    successful: bool
-    features: typing.List[_FeatureResponse]
-
-
-class SpeechesResponse(SQLModel):
-    """Speeches response fields."""
-
-    id: uuid.UUID
     title: str
     text: str
     date: datetime.date
-    created_at: datetime.datetime
     URL: HttpUrl
-    features: typing.Optional[typing.List[_FeatureResponse]] = None
+    category: typing.Optional[str] = Field(default=None)
+
+class ResponseMeta(SQLModel):
+    """Metadata response model."""
+    id: uuid.UUID
+    created_at: datetime.datetime
+    date: datetime.date
+    title: str
+    URL: HttpUrl
+
+# Speeches
+# read_speeches
+class SpeachesReadResponse(SQLModel):
+    id: uuid.UUID
+
+class ResponseMF(ResponseMeta):
+    """Metadata + features response model."""
+    # extras
+    features: typing.List[Features] = []
+
+class ResponseMT(ResponseMeta):
+    """Metadata + text response model."""
+    # extras
+    # defining text as str as opposed to Texts
+    # because I only care about Texts.text field
+    text: str
+
+class ResponseMTF(ResponseMeta):
+    text: str
+    features: typing.List[Features] = []
