a92a1c22a86c705263ec1fe87d3836ef4fa9adac
hp0404 <hryhorii.pavlenko@gmail.com>
2023-01-30 11:18:47 +0200

    add red_lines endpoint

---
+++ app/api/api_v1/endpoints/red_lines.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""This module contains /classification router."""
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session
+
+from app.db import get_session
+from app.helpers.red_lines import RedLinesClassifier
+from app.models import RedLines as database_model
+from app.schemas import RedLines as response_model
+
+router = APIRouter(prefix="/classifier", tags=["NLP pipeline"])
+classifier = RedLinesClassifier()
+
+
+@router.post("/predict", response_model=response_model)
+def predict_text(text: str):
+    return classifier.store(text)
+
+
+@router.get("/{id}", response_model=response_model)
+def read_prediction(id: int, session: Session = Depends(get_session)):
+    data = session.get(database_model, id)
+    if not data:
+        raise HTTPException(status_code=404, detail="Prediction not found")
+    return data
